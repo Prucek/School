c4c1a1c0c3bed53d2ac3ffff678a789dc0f3ca1e
Prucek <56258989+Prucek@users.noreply.github.com>
2020-03-18 11:07:50 +0100

    Add files via upload

--- IOS/dirgraph.sh
+++ IOS/dirgraph.sh
@@ -2,18 +2,69 @@
 
 POSIXLY_CORRECT=yes
 
-echo Root directory: $(pwd)
-echo Directories: $(ls -lR | grep "^d" | wc -l)
-echo All files: $(find $(pwd) -type f | wc -l)
-echo File size histogram:
-y=$(ls -lR | grep "^-" | sed -E 's/([^0-9]*([0-9]*)){2}.*/\2/')
+FSHIST() #1list_of_sizes
+{   
+    declare -a list
+    list[0]=100 
+    list[1]=1024 
+    list[2]=10240 
+    list[3]=102400 
+    list[4]=1048576 
+    list[5]=10485760 
+    list[6]=104857600 
+    list[7]=1073741824
 
-echo \<100 B
-echo \<1 KiB
-echo \<10 KiB
-echo \<100 KiB
-echo \<1 MiB
-echo \<10 MiB
-echo \<100 MiB
-echo \<1 GiB
-echo \>=1 GiB
+    declare -a to_print
+    to_print[0]="<100 B  "
+    to_print[1]="<1 KiB  "
+    to_print[2]="<10 KiB "
+    to_print[3]="<100 KiB" 
+    to_print[4]="<1 MiB  "
+    to_print[5]="<10 MiB " 
+    to_print[6]="<100 MiB" 
+    to_print[7]="<1 GiB  "
+    to_print[8]=">=1 GiB "
+
+    arg=$@
+    sum=0
+    for i in {0..8}
+    do
+        count=0
+        for j in $arg
+        do
+            if test $i -eq 8; then
+                if test $j -gt ${list[7]}; then
+                    ((count++))
+                fi 
+            elif test $j -lt ${list[i]}; then
+                ((count++))
+            fi
+            shift
+        done
+    
+        count=$((count-sum))
+        sum=$((sum+count))
+
+        echo -n "  ${to_print[i]}: "
+        for (( k=1; k<=$count; k++ ))
+        do
+            echo -n \#
+        done
+        echo
+    done
+
+}
+
+#printing
+DIR=$(pwd)
+ND=$(ls -lR | grep "^d" | wc -l)
+NF=$(find $(pwd) -type f | wc -l)
+echo Root directory: $DIR
+echo Directories: $ND 
+echo All files: $NF
+echo File size histogram:
+
+list_of_sizes=$(ls -lR | grep "^-" | sed -E 's/([^0-9]*([0-9]*)){2}.*/\2/')
+FSHIST $list_of_sizes
+
+return 0
