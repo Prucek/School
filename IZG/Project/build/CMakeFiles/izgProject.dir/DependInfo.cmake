0076361851b5ea0db0a027b69717f419afdab093
Peter <peter.rucek@gmail.com>
2021-04-25 23:23:35 +0200

    Add initial IZG project 3.7 points

---
+++ IZG/Project/build/CMakeFiles/izgProject.dir/DependInfo.cmake
@@ -0,0 +1,77 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/peter/Downloads/git/School/IZG/Project/examples/czFlagMethod.cpp" "/home/peter/Downloads/git/School/IZG/Project/build/CMakeFiles/izgProject.dir/examples/czFlagMethod.cpp.o"
+  "/home/peter/Downloads/git/School/IZG/Project/examples/modelMethod.cpp" "/home/peter/Downloads/git/School/IZG/Project/build/CMakeFiles/izgProject.dir/examples/modelMethod.cpp.o"
+  "/home/peter/Downloads/git/School/IZG/Project/examples/phongMethod.cpp" "/home/peter/Downloads/git/School/IZG/Project/build/CMakeFiles/izgProject.dir/examples/phongMethod.cpp.o"
+  "/home/peter/Downloads/git/School/IZG/Project/examples/skFlagMethod.cpp" "/home/peter/Downloads/git/School/IZG/Project/build/CMakeFiles/izgProject.dir/examples/skFlagMethod.cpp.o"
+  "/home/peter/Downloads/git/School/IZG/Project/examples/texturedQuadMethod.cpp" "/home/peter/Downloads/git/School/IZG/Project/build/CMakeFiles/izgProject.dir/examples/texturedQuadMethod.cpp.o"
+  "/home/peter/Downloads/git/School/IZG/Project/examples/triangle3DMethod.cpp" "/home/peter/Downloads/git/School/IZG/Project/build/CMakeFiles/izgProject.dir/examples/triangle3DMethod.cpp.o"
+  "/home/peter/Downloads/git/School/IZG/Project/examples/triangleBufferMethod.cpp" "/home/peter/Downloads/git/School/IZG/Project/build/CMakeFiles/izgProject.dir/examples/triangleBufferMethod.cpp.o"
+  "/home/peter/Downloads/git/School/IZG/Project/examples/triangleClip1Method.cpp" "/home/peter/Downloads/git/School/IZG/Project/build/CMakeFiles/izgProject.dir/examples/triangleClip1Method.cpp.o"
+  "/home/peter/Downloads/git/School/IZG/Project/examples/triangleClip2Method.cpp" "/home/peter/Downloads/git/School/IZG/Project/build/CMakeFiles/izgProject.dir/examples/triangleClip2Method.cpp.o"
+  "/home/peter/Downloads/git/School/IZG/Project/examples/triangleMethod.cpp" "/home/peter/Downloads/git/School/IZG/Project/build/CMakeFiles/izgProject.dir/examples/triangleMethod.cpp.o"
+  "/home/peter/Downloads/git/School/IZG/Project/framework/application.cpp" "/home/peter/Downloads/git/School/IZG/Project/build/CMakeFiles/izgProject.dir/framework/application.cpp.o"
+  "/home/peter/Downloads/git/School/IZG/Project/framework/bunny.cpp" "/home/peter/Downloads/git/School/IZG/Project/build/CMakeFiles/izgProject.dir/framework/bunny.cpp.o"
+  "/home/peter/Downloads/git/School/IZG/Project/framework/main.cpp" "/home/peter/Downloads/git/School/IZG/Project/build/CMakeFiles/izgProject.dir/framework/main.cpp.o"
+  "/home/peter/Downloads/git/School/IZG/Project/framework/model.cpp" "/home/peter/Downloads/git/School/IZG/Project/build/CMakeFiles/izgProject.dir/framework/model.cpp.o"
+  "/home/peter/Downloads/git/School/IZG/Project/framework/textureData.cpp" "/home/peter/Downloads/git/School/IZG/Project/build/CMakeFiles/izgProject.dir/framework/textureData.cpp.o"
+  "/home/peter/Downloads/git/School/IZG/Project/framework/window.cpp" "/home/peter/Downloads/git/School/IZG/Project/build/CMakeFiles/izgProject.dir/framework/window.cpp.o"
+  "/home/peter/Downloads/git/School/IZG/Project/libs/impl.cpp" "/home/peter/Downloads/git/School/IZG/Project/build/CMakeFiles/izgProject.dir/libs/impl.cpp.o"
+  "/home/peter/Downloads/git/School/IZG/Project/student/drawModel.cpp" "/home/peter/Downloads/git/School/IZG/Project/build/CMakeFiles/izgProject.dir/student/drawModel.cpp.o"
+  "/home/peter/Downloads/git/School/IZG/Project/student/gpu.cpp" "/home/peter/Downloads/git/School/IZG/Project/build/CMakeFiles/izgProject.dir/student/gpu.cpp.o"
+  "/home/peter/Downloads/git/School/IZG/Project/tests/clippingTests.cpp" "/home/peter/Downloads/git/School/IZG/Project/build/CMakeFiles/izgProject.dir/tests/clippingTests.cpp.o"
+  "/home/peter/Downloads/git/School/IZG/Project/tests/conformanceTests.cpp" "/home/peter/Downloads/git/School/IZG/Project/build/CMakeFiles/izgProject.dir/tests/conformanceTests.cpp.o"
+  "/home/peter/Downloads/git/School/IZG/Project/tests/drawModelTests.cpp" "/home/peter/Downloads/git/School/IZG/Project/build/CMakeFiles/izgProject.dir/tests/drawModelTests.cpp.o"
+  "/home/peter/Downloads/git/School/IZG/Project/tests/finalImageTest.cpp" "/home/peter/Downloads/git/School/IZG/Project/build/CMakeFiles/izgProject.dir/tests/finalImageTest.cpp.o"
+  "/home/peter/Downloads/git/School/IZG/Project/tests/fragmentShaderTests.cpp" "/home/peter/Downloads/git/School/IZG/Project/build/CMakeFiles/izgProject.dir/tests/fragmentShaderTests.cpp.o"
+  "/home/peter/Downloads/git/School/IZG/Project/tests/performanceTest.cpp" "/home/peter/Downloads/git/School/IZG/Project/build/CMakeFiles/izgProject.dir/tests/performanceTest.cpp.o"
+  "/home/peter/Downloads/git/School/IZG/Project/tests/renderMethodFrame.cpp" "/home/peter/Downloads/git/School/IZG/Project/build/CMakeFiles/izgProject.dir/tests/renderMethodFrame.cpp.o"
+  "/home/peter/Downloads/git/School/IZG/Project/tests/saveFrame.cpp" "/home/peter/Downloads/git/School/IZG/Project/build/CMakeFiles/izgProject.dir/tests/saveFrame.cpp.o"
+  "/home/peter/Downloads/git/School/IZG/Project/tests/takeScreenShot.cpp" "/home/peter/Downloads/git/School/IZG/Project/build/CMakeFiles/izgProject.dir/tests/takeScreenShot.cpp.o"
+  "/home/peter/Downloads/git/School/IZG/Project/tests/testCommon.cpp" "/home/peter/Downloads/git/School/IZG/Project/build/CMakeFiles/izgProject.dir/tests/testCommon.cpp.o"
+  "/home/peter/Downloads/git/School/IZG/Project/tests/vertexShaderTests.cpp" "/home/peter/Downloads/git/School/IZG/Project/build/CMakeFiles/izgProject.dir/tests/vertexShaderTests.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "CMAKE_ROOT_DIR=\"/home/peter/Downloads/git/School/IZG/Project\""
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../"
+  "../libs/json"
+  "../libs/stb_image"
+  "../libs/SDL2-2.0.14/include"
+  "../libs/ArgumentViewer/src"
+  "libs/ArgumentViewer"
+  "../libs/ArgumentViewer"
+  "../libs/MealyMachine/src"
+  "libs/MealyMachine"
+  "../libs/MealyMachine"
+  "../libs/TxtUtils/src"
+  "libs/TxtUtils"
+  "../libs/TxtUtils"
+  "../libs/BasicCamera/src"
+  "libs/BasicCamera"
+  "../libs/BasicCamera"
+  "../libs/glm-0.9.9.8"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/peter/Downloads/git/School/IZG/Project/build/libs/SDL2-2.0.14/CMakeFiles/SDL2-static.dir/DependInfo.cmake"
+  "/home/peter/Downloads/git/School/IZG/Project/build/libs/SDL2-2.0.14/CMakeFiles/SDL2main.dir/DependInfo.cmake"
+  "/home/peter/Downloads/git/School/IZG/Project/build/libs/ArgumentViewer/CMakeFiles/ArgumentViewer.dir/DependInfo.cmake"
+  "/home/peter/Downloads/git/School/IZG/Project/build/libs/BasicCamera/CMakeFiles/BasicCamera.dir/DependInfo.cmake"
+  "/home/peter/Downloads/git/School/IZG/Project/build/libs/TxtUtils/CMakeFiles/TxtUtils.dir/DependInfo.cmake"
+  "/home/peter/Downloads/git/School/IZG/Project/build/libs/MealyMachine/CMakeFiles/MealyMachine.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ IZG/Project/build/libs/ArgumentViewer/CMakeFiles/ArgumentViewer.dir/DependInfo.cmake
@@ -0,0 +1,41 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/peter/Downloads/git/School/IZG/Project/libs/ArgumentViewer/src/ArgumentViewer/ArgumentViewer.cpp" "/home/peter/Downloads/git/School/IZG/Project/build/libs/ArgumentViewer/CMakeFiles/ArgumentViewer.dir/src/ArgumentViewer/ArgumentViewer.cpp.o"
+  "/home/peter/Downloads/git/School/IZG/Project/libs/ArgumentViewer/src/ArgumentViewer/private/ArgumentListFormat.cpp" "/home/peter/Downloads/git/School/IZG/Project/build/libs/ArgumentViewer/CMakeFiles/ArgumentViewer.dir/src/ArgumentViewer/private/ArgumentListFormat.cpp.o"
+  "/home/peter/Downloads/git/School/IZG/Project/libs/ArgumentViewer/src/ArgumentViewer/private/ArgumentViewerImpl.cpp" "/home/peter/Downloads/git/School/IZG/Project/build/libs/ArgumentViewer/CMakeFiles/ArgumentViewer.dir/src/ArgumentViewer/private/ArgumentViewerImpl.cpp.o"
+  "/home/peter/Downloads/git/School/IZG/Project/libs/ArgumentViewer/src/ArgumentViewer/private/CommonFunctions.cpp" "/home/peter/Downloads/git/School/IZG/Project/build/libs/ArgumentViewer/CMakeFiles/ArgumentViewer.dir/src/ArgumentViewer/private/CommonFunctions.cpp.o"
+  "/home/peter/Downloads/git/School/IZG/Project/libs/ArgumentViewer/src/ArgumentViewer/private/ContextFormat.cpp" "/home/peter/Downloads/git/School/IZG/Project/build/libs/ArgumentViewer/CMakeFiles/ArgumentViewer.dir/src/ArgumentViewer/private/ContextFormat.cpp.o"
+  "/home/peter/Downloads/git/School/IZG/Project/libs/ArgumentViewer/src/ArgumentViewer/private/Format.cpp" "/home/peter/Downloads/git/School/IZG/Project/build/libs/ArgumentViewer/CMakeFiles/ArgumentViewer.dir/src/ArgumentViewer/private/Format.cpp.o"
+  "/home/peter/Downloads/git/School/IZG/Project/libs/ArgumentViewer/src/ArgumentViewer/private/Globals.cpp" "/home/peter/Downloads/git/School/IZG/Project/build/libs/ArgumentViewer/CMakeFiles/ArgumentViewer.dir/src/ArgumentViewer/private/Globals.cpp.o"
+  "/home/peter/Downloads/git/School/IZG/Project/libs/ArgumentViewer/src/ArgumentViewer/private/IsPresentFormat.cpp" "/home/peter/Downloads/git/School/IZG/Project/build/libs/ArgumentViewer/CMakeFiles/ArgumentViewer.dir/src/ArgumentViewer/private/IsPresentFormat.cpp.o"
+  "/home/peter/Downloads/git/School/IZG/Project/libs/ArgumentViewer/src/ArgumentViewer/private/LineSplitter.cpp" "/home/peter/Downloads/git/School/IZG/Project/build/libs/ArgumentViewer/CMakeFiles/ArgumentViewer.dir/src/ArgumentViewer/private/LineSplitter.cpp.o"
+  "/home/peter/Downloads/git/School/IZG/Project/libs/ArgumentViewer/src/ArgumentViewer/private/StringVectorFormat.cpp" "/home/peter/Downloads/git/School/IZG/Project/build/libs/ArgumentViewer/CMakeFiles/ArgumentViewer.dir/src/ArgumentViewer/private/StringVectorFormat.cpp.o"
+  "/home/peter/Downloads/git/School/IZG/Project/libs/ArgumentViewer/src/ArgumentViewer/private/ValueFormat.cpp" "/home/peter/Downloads/git/School/IZG/Project/build/libs/ArgumentViewer/CMakeFiles/ArgumentViewer.dir/src/ArgumentViewer/private/ValueFormat.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "libs/ArgumentViewer"
+  "../libs/ArgumentViewer"
+  "../libs/ArgumentViewer/src"
+  "../libs/MealyMachine/src"
+  "libs/MealyMachine"
+  "../libs/MealyMachine"
+  "../libs/TxtUtils/src"
+  "libs/TxtUtils"
+  "../libs/TxtUtils"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/peter/Downloads/git/School/IZG/Project/build/libs/MealyMachine/CMakeFiles/MealyMachine.dir/DependInfo.cmake"
+  "/home/peter/Downloads/git/School/IZG/Project/build/libs/TxtUtils/CMakeFiles/TxtUtils.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
